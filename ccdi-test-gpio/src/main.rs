d7104cd11186383e474c7f95ce21206e909dd845
Viktor Chvatal <viktor@chvatal.name>
2023-08-05 12:50:51 +0200

    Added GPIO test app.

---
+++ ccdi-test-gpio/src/main.rs
@@ -0,0 +1,38 @@
+// ============================================ PUBLIC =============================================
+
+use std::{env::args, path::{PathBuf, Path}, thread, time::Duration};
+
+use ccdi_common::{save_text_file, log_err};
+
+fn main() {
+    let path = match args().skip(1).next() {
+        Some(path) => path,
+        None => {
+            println!("Go argument passed, exiting.");
+            return;
+        }
+    };
+
+    println!("Testing GPIO on: {}", path);
+    let path = PathBuf::from(path);
+
+    loop {
+        println!("Writing True");
+        log_err("write_output", write_output(&path, true));
+        thread::sleep(Duration::from_secs(1));
+
+        println!("Writing False");
+        log_err("write_output", write_output(&path, false));
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+pub fn write_output(path: &Path, value: bool) -> Result<(), String> {
+    save_text_file(
+        match value {
+            false => "0\n",
+            true => "1\n",
+        },
+        path
+    )
+}
